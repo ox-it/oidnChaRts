434d33b80ae027cd3bfb4878f2ef2af83c57b583
martinjhnhadley <martinjohnhadley@gmail.com>
2016-10-11 12:36:18 +0100

    data_stacked_bar_chart is now exported in long format

--- data-raw/data_charts.R
+++ data-raw/data_charts.R
@@ -1,6 +1,15 @@
 data_stacked_bar_chart <- data.frame(
   "Country" = c(
-    "United Kingdom","France","Saudi Arabia","Egypt","Germany","China","Slovakia","Canada","Estonia","Ireland"
+    "United Kingdom",
+    "France",
+    "Saudi Arabia",
+    "Egypt",
+    "Germany",
+    "China",
+    "Slovakia",
+    "Canada",
+    "Estonia",
+    "Ireland"
   ),
   "Business" =  c(8, 6, 7, 8, 9, 7, 9, 7, 9, 9),
   "Overlay" = c(22, 28, 11, 19, 11, 39, 22, 10, 30, 7),
@@ -9,4 +18,11 @@ data_stacked_bar_chart <- data.frame(
   "Unclassified" = c(42, 48, 50, 45, 49, 40, 42, 47, 48, 44),
   stringsAsFactors = F
 )
-save(data_stacked_bar_chart, file = "data/data_stacked_bar_chart.rdata")
+
+save(data_stacked_bar_chart %>%
+       gather("Activity",
+              "Hours",
+              which(
+                colnames(data_stacked_bar_chart) %in% setdiff(colnames(data_stacked_bar_chart), "Country")
+              )),
+     file = "data/data_stacked_bar_chart.rdata")
