32bd6300770be22e1095a3de86408171eb69b9b5
martinjhnhadley <martinjohnhadley@gmail.com>
2016-12-09 22:01:00 +0000

    Added additional documentation

--- R/maps.R
+++ R/maps.R
@@ -2,6 +2,21 @@
 #'
 #' \code{geo_lines_map} creates a map with lines (great circles) between geo graphic locations using the specified library, which can be used in the library's \%>\% workflow. Data must be provided in long format.
 #'
+#' @param data A data.frame with sender-receiver pairs, needs the following columns
+#' \itemize{
+#'  \item{"sender.longitude"}{ : sender longitude}
+#'  \item{"sender.latitude"}{ : sender latitude}
+#'  \item{"receiver.longitude"}{ : receiver longitude}
+#'  \item{"receiver.latitude"}{ : receiver latitude}
+#'  }
+#'
+#'  @param library must be one of the supported libraries, currently; "leaflet". Defaults to "leaflet".
+#'  @param line.color color for the geolines (great circles), defaults to #2c7bb6
+#'  @param line.popup expression used to populate popups displayed when a geoline is clicked, can use models/formula. For instance, ~paste(sender.longitude, receiver.longitude)
+#'  @param sender.color color of the dots representing sender locations, defaults to #fdae61
+#'  @param receiver.color color of the dots representing receiver locations, defaults to #d7191c
+#'  @param line.options named list of options for geolines
+#'  @param termini.options named list of options for the termini (sender/receiver dots)
 #'
 #' @export
 geo_lines_map <-
@@ -76,5 +91,44 @@ leaflet_geo_lines_map <- function(data = NA,
       color = optnl.args$receiver.color,
       opacity = 0.6
     )
-
 }
+
+
+
+#' addLegendCustom
+addLegendCustom <-
+  function(map, colors, labels, sizes, opacity = 0.5) {
+    ## Inspired by http://stackoverflow.com/a/37482936/1659890
+    colorAdditions <-
+      paste0(colors, "; width:", sizes, "px; height:", sizes, "px")
+    labelAdditions <-
+      paste0(
+        "<div style='display: inline-block;height: ",
+        sizes,
+        "px;margin-top: 4px;line-height: ",
+        sizes,
+        "px;'>",
+        labels,
+        "</div>"
+      )
+
+    return(addLegend(
+      map,
+      colors = colorAdditions,
+      labels = labelAdditions,
+      opacity = opacity
+    ))
+  }
+
+#' leaflet_termini_legend
+leaflet_termini_legend <- function(map = NA, colors = NA,
+                                   optnl.args = NA){
+  map %>%
+    addLegendCustom(
+      colors = c("#fdae61", "#d7191c"),
+      labels = c("Sender", "Receiver"),
+      sizes = c(10, 10)
+    )
+}
+
+
