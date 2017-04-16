2015ce69f555ea3f9187f2af9ea5ddf0d7e334cd
martinjhnhadley <martinjohnhadley@gmail.com>
2017-04-16 20:40:52 +0100

    data_stacked_bar_chart is now based on iLabour project data
    Previously a fake dataset was used, now a real dataset based on
    https://doi.org/10.6084/m9.figshare.3761562 is used

--- data-raw/data_charts.R
+++ data-raw/data_charts.R
@@ -1,39 +1,39 @@
 library(tidyverse)
 
-data_stacked_bar_chart <- data.frame(
-  "country" = c(
-    "United Kingdom",
-    "France",
-    "Saudi Arabia",
-    "Egypt",
-    "Germany",
-    "China",
-    "Slovakia",
-    "Canada",
-    "Estonia",
-    "Ireland"
-  ),
-  "Business" =  c(8, 6, 7, 8, 9, 7, 9, 7, 9, 9),
-  "Other" = c(22, 28, 11, 19, 11, 39, 22, 10, 30, 7),
-  "Personal Visit" = c(19, 21, 21, 20, 19, 19, 21, 21, 20, 20),
-  "Teleconference" = c(9, 29, 21, 44, 59, 11, 53, 16, 49, 36),
-  "Unclassified" = c(42, 48, 50, 45, 49, 40, 42, 47, 48, 44),
-  stringsAsFactors = F
-)
-# Columns
-categories_column <- "country"
-subcategories_column <- "activity"
-value_column <- "hours"
-# Convert to long-format
-data_stacked_bar_chart <- data_stacked_bar_chart %>%
-  gather(activity, hours, match(
-    setdiff(colnames(data_stacked_bar_chart), categories_column),
-    colnames(data_stacked_bar_chart)
-  )) %>%
-  as_data_frame()
-
-save(data_stacked_bar_chart,
-     file = "data/data_stacked_bar_chart.rdata")
+# data_stacked_bar_chart <- data.frame(
+#   "country" = c(
+#     "United Kingdom",
+#     "France",
+#     "Saudi Arabia",
+#     "Egypt",
+#     "Germany",
+#     "China",
+#     "Slovakia",
+#     "Canada",
+#     "Estonia",
+#     "Ireland"
+#   ),
+#   "Business" =  c(8, 6, 7, 8, 9, 7, 9, 7, 9, 9),
+#   "Other" = c(22, 28, 11, 19, 11, 39, 22, 10, 30, 7),
+#   "Personal Visit" = c(19, 21, 21, 20, 19, 19, 21, 21, 20, 20),
+#   "Teleconference" = c(9, 29, 21, 44, 59, 11, 53, 16, 49, 36),
+#   "Unclassified" = c(42, 48, 50, 45, 49, 40, 42, 47, 48, 44),
+#   stringsAsFactors = F
+# )
+# # Columns
+# categories_column <- "country"
+# subcategories_column <- "activity"
+# value_column <- "hours"
+# # Convert to long-format
+# data_stacked_bar_chart <- data_stacked_bar_chart %>%
+#   gather(activity, hours, match(
+#     setdiff(colnames(data_stacked_bar_chart), categories_column),
+#     colnames(data_stacked_bar_chart)
+#   )) %>%
+#   as_data_frame()
+# 
+# # save(data_stacked_bar_chart,
+# #      file = "data/data_stacked_bar_chart.rdata")
 
 library(tidyverse)
 
