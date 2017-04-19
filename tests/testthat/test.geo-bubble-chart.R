2dc95dbf8505a8f1e90633f2ce814cac668ba617
martinjhnhadley <martinjohnhadley@gmail.com>
2017-04-19 17:55:46 +0100

    fixed test_that(geo_bubble_chart)

--- tests/testthat/test.geo-bubble-chart.R
+++ tests/testthat/test.geo-bubble-chart.R
@@ -1,9 +1,19 @@
 test_that("geo_bubble_chart(library='leaflet') makes a leaflet object",
           {
             chart <- data_geo_bubble_chart %>%
-              geo_bubble_chart(library = "leaflet",
-                               bubble.group = ~country,
-                               bubble.radius = ~count)
+              filter(count > 5) %>%
+              mutate(count = scales::rescale(count, to = c(3,20))) %>%
+              mutate(color = plyr::mapvalues(
+                country,
+                from = unique(country),
+                to = RColorBrewer::brewer.pal(length(unique(country)), "Dark2")
+              )) %>%
+              geo_bubble_chart(
+                library = "leaflet",
+                bubble.radius = ~count,
+                bubble.group = ~country,
+                color = ~color
+              )
             
             expect_true(all(class(chart) %in% c("leaflet",  "htmlwidget")))
             
@@ -13,9 +23,19 @@ test_that("geo_bubble_chart(library='leaflet') makes a leaflet object",
 test_that("geo_bubble_chart(library='highcharter') makes a highcharter object",
           {
             chart <- data_geo_bubble_chart %>%
-              geo_bubble_chart(library = "highcharter",
-                               bubble.group = ~country,
-                               bubble.radius = ~count)
+              filter(count > 5) %>%
+              mutate(count = scales::rescale(count, to = c(3,20))) %>%
+              mutate(color = plyr::mapvalues(
+                country,
+                from = unique(country),
+                to = RColorBrewer::brewer.pal(length(unique(country)), "Dark2")
+              )) %>%
+              geo_bubble_chart(
+                library = "highcharter",
+                bubble.radius = ~count,
+                bubble.group = ~country,
+                color = ~color
+              )
             
             expect_true(all(class(chart) %in% c("highchart",  "htmlwidget")))
             
@@ -24,9 +44,19 @@ test_that("geo_bubble_chart(library='highcharter') makes a highcharter object",
 test_that("geo_bubble_chart(library='plotly') makes a plotly object",
           {
             chart <- data_geo_bubble_chart %>%
-              geo_bubble_chart(library = "plotly",
-                               bubble.group = ~country,
-                               bubble.radius = ~count)
+              filter(count > 5) %>%
+              mutate(count = scales::rescale(count, to = c(3,20))) %>%
+              mutate(color = plyr::mapvalues(
+                country,
+                from = unique(country),
+                to = RColorBrewer::brewer.pal(length(unique(country)), "Dark2")
+              )) %>%
+              geo_bubble_chart(
+                library = "plotly",
+                bubble.radius = ~count,
+                bubble.group = ~country,
+                color = ~color
+              )
             
             expect_true(all(class(chart) %in% c("plotly",  "htmlwidget")))
             
