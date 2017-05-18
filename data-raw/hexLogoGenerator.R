b25d8cae4285793cda87edd38c541ed94436181b
martinjhnhadley <martinjohnhadley@gmail.com>
2017-05-18 13:52:57 +0100

    Added attribution for logo

--- data-raw/hexLogoGenerator.R
+++ data-raw/hexLogoGenerator.R
@@ -2,7 +2,7 @@
 # biocLite("EBImage")
 
 library(hexSticker)
-
+## Please note the "all of the meme" is inspired by the work of "Hyperbole and a Half" here: http://hyperboleandahalf.blogspot.com/2010/06/this-is-why-ill-never-be-adult.html
 imgPath <- "data-raw/all-of-the-htmlwidgets.png"
 
 sticker(imgPath, package="oidnChaRts", p_size=8, s_x=1, s_y=.75, s_width=.6,
